c53a97a588aa956da63297d4a604e181d10eba5c
Aradhanadevi <aradhanajadeja81@gmail.com>
2025-04-29 21:45:47 +0530

    .

--- assets/assets.js
+++ assets/assets.js
@@ -102,7 +102,7 @@ export const workData = [
 export const serviceData = [
     { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
     { icon: assets.mobile_icon, title: 'Android App Delevlopment', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
+    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience. so I made a portfolio where it has my all design.', link: '' },
     { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
 ]
 
